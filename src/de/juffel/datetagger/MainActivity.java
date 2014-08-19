70af53c815e0dbc3959cc45b1c3848806cd87d50
Julian Dobmann <julian.dobmann@gmail.com>
2014-08-19 19:22:43 +0200

    Did some strucutral schangessh

--- src/de/juffel/datetagger/MainActivity.java
+++ src/de/juffel/datetagger/MainActivity.java
@@ -99,27 +99,16 @@ public class MainActivity extends Activity {
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
+            selectDrawerItem(position);
         }
     }
 
-    private void selectItem(int position) {
-    	// do something!
-//        // Create a new fragment and specify the planet to show based on position
-//        Fragment fragment = new PlanetFragment();
-//        Bundle args = new Bundle();
-//        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-//        fragment.setArguments(args);
-//
-//        // Insert the fragment by replacing any existing fragment
-//        FragmentManager fragmentManager = getFragmentManager();
-//        fragmentManager.beginTransaction()
-//                       .replace(R.id.content_frame, fragment)
-//                       .commit();
-//
-//        // Highlight the selected item, update the title, and close the drawer
-//        mDrawerList.setItemChecked(position, true);
-//        setTitle(mPlanetTitles[position]);
+    /**
+     * Method gets called, when the user clicks in the drawer
+     * @param position
+     */
+    private void selectDrawerItem(int position) {
+    	// do something
         mDrawerLayout.closeDrawer(mDrawerList);
     }
 }
